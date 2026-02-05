815b10179e135f5b1656a2908683858254453714
朱重迁 <z32664206862022@163.com>
2026-02-05 17:49:21 +0800

    style: 优化页面滚动条样式并重构头部导航

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,41 +1,11 @@
 <template>
-  <!-- <header class="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
-    <div class="container flex h-14 items-center">
-      <div class="flex items-center gap-2 mr-4">
-        <Link href="/" class="flex items-center gap-2">
-          <span class="text-lg font-bold">中文博客</span>
-        </Link>
-      </div>
-      <div>
-        <div class="relative flex items-center w-full" v-if="showSearch">
-          <Input type="search" placeholder="Search..." autoFocus />
-          <Button variant="ghost" size="icon" class="absolute right-0" @click="setShowSearch(false)">
-            <X class="h-4 w-4" />
-            <span class="sr-only">Close search</span>
-          </Button>
-        </div>
-        <template v-else>
-          <Button variant="ghost" size="icon" @click="setShowSearch(true)">
-            <Search class="h-5 w-5" />
-            <span class="sr-only">Search</span>
-          </Button>
-        </template>
-      </div>
-
-      <nav class="hidden md:flex md:gap-6 flex-1 justify-center">
-        <div v-for="item in navItems">
-          <NuxtLink :to="item.href" class="flex items-center gap-2">
-            <span>{{ item.name }}</span>
-          </NuxtLink>
-        </div>
-      </nav>
-
-      <ThemeToggle></ThemeToggle>
-    </div>
-  </header>
-   -->
   <UHeader>
     <template #title>
+      <div class="flex items-center gap-2 mr-4">
+        <NuxtLink href="/" class="flex items-center gap-2">
+          <span class="text-lg font-bold">中文博客</span>
+        </NuxtLink>
+      </div>
       <Logo class="h-6 w-auto" />
     </template>
 
@@ -64,40 +34,18 @@ import type { NavigationMenuItem } from '@nuxt/ui'
 const route = useRoute()
 
 const items = computed<NavigationMenuItem[]>(() => [
+  { label: 'Home', to: '/' },
+  { label: '文章', to: '/blog/1' },
+  { label: '时光轴', to: '/timeline' },
   {
-    label: 'Docs',
-    to: '/docs/getting-started',
-    active: route.path.startsWith('/docs/getting-started')
+    label: '影像长廊',
+    to: '/album'
   },
-  {
-    label: 'Components',
-    to: '/docs/components',
-    active: route.path.startsWith('/docs/components')
-  },
-  {
-    label: 'Figma',
-    to: 'https://go.nuxt.com/figma-ui',
-    target: '_blank'
-  },
-  {
-    label: 'Releases',
-    to: 'https://github.com/nuxt/ui/releases',
-    target: '_blank'
-  }
+  { label: '音乐库', to: '/music-player' },
+  { label: '第三方库', to: '/libraries' },
+  { label: '资源库', to: '/chat' },
+  { label: '关于', to: '/about' }
 ])
-const navItems = [
-  { name: 'Home', href: '/' },
-  { name: '文章', href: '/blog/1' },
-  { name: '时光轴', href: '/timeline' },
-  {
-    name: '影像长廊',
-    href: '/album'
-  },
-  { name: '音乐库', href: '/music-player' },
-  { name: '第三方库', href: '/libraries' },
-  { name: '资源库', href: '/chat' },
-  { name: '关于', href: '/about' }
-]
 
 const showSearch = ref(false)
 const setShowSearch = (val: boolean) => {
